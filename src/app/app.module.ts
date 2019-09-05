b5d1ae667f2b1ef27b19a87ed8940e68ffdbb543
David Bousquet <david@programaciondb.cl>
2019-09-05 15:09:40 -0400

    se crea componente menu-app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatriculasComponent } from './componentes/matriculas/matriculas.component';
 import { TalleresComponent } from './componentes/talleres/talleres.component';
+import { MenuAppComponent } from './componentes/menu-app/menu-app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MatriculasComponent,
-    TalleresComponent
+    TalleresComponent,
+    MenuAppComponent
   ],
   imports: [
     BrowserModule,
